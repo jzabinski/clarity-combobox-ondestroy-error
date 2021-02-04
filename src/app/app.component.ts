7382e27acd757ba51d16fdb6ee398035ea72021b
Joe Zabinski <jzabinski@meditech.com>
2021-02-04 12:00:38 -0500

    Added supporting functionality for error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'clarity-combobox-ondestroy-bug';
+export class AppComponent  {
+  letterCombos = ['ab', 'ac', 'ad'];
+  currentCombo: string;
+  display: string;
+  get selected(): string {
+    return this.currentCombo;
+  }
+  set selected(combo: string) {
+    console.log(combo);
+    if (combo === null) {
+      this.display = 'emitted null!';
+    }
+    else {
+      this.display = combo;
+    }
+    this.currentCombo = combo;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
